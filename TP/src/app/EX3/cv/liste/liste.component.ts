3357f07d3170fe31791de0067090becdeb981043
jihedbenamara <jihedbenamara02@gmail.com>
2024-11-22 15:54:58 +0100

    final ex3

--- TP/src/app/EX3/cv/liste/liste.component.ts
+++ TP/src/app/EX3/cv/liste/liste.component.ts
@@ -18,13 +18,37 @@ export class ListeComponent {
     job:'Student'
   },
   {
-    id:1,
+    id:2,
     firstName:'ahmed',
     lastName:'trigui',
     image:'../../../assets/ahmed.jpg',
     age:22,
     cin:14424645,
-    job:'Student'} ];
+    job:'Student'},
+    {
+      id:3,
+      firstName:'houaida',
+      lastName:'mangour',
+      image:'../../../assets/houaida.jpg',
+      age:22,
+      cin:14424646,
+      job:'Student'},
+      {
+        id:1,
+        firstName:'rahma',
+        lastName:'heni',
+        image:'../../../assets/rahma.jpg',
+        age:22,
+        cin:14424647,
+        job:'Student'},
+        {
+          id:1,
+          firstName:'anas',
+          lastName:'bader',
+          image:'../../../assets/anas.jpg',
+          age:22,
+          cin:14424648,
+          job:'Student'} ];
 
   onItemClicked(item: any) {
     this.itemSelected.emit(item); // Emits the selected item to parent
--- TP/src/app/app-routing.module.ts
+++ TP/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CvComponent } from './EX3/cv/cv.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'cv', component: CvComponent }, 
+  { path: '', redirectTo: '', pathMatch: 'full' }, 
+  { path: '**', redirectTo: '/cv' } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
